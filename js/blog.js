bbd6b4a264e9888329cdddf1efa58aa22371295e
Sergio <281540+sergio97@users.noreply.github.com>
2019-01-01 14:56:05 -0500

    Convert tabs to spaces and \r\n to \n

--- js/blog.js
+++ js/blog.js
@@ -16,171 +16,171 @@
 
 $(document).ready(function()
 {
-	"use strict";
+  "use strict";
 
-	/* 
+  /*
 
-	1. Vars and Inits
+  1. Vars and Inits
 
-	*/
+  */
 
-	var ctrl = new ScrollMagic.Controller();
+  var ctrl = new ScrollMagic.Controller();
 
-	setHeader();
-	initMenu();
-	initFavs();
-	initSvg();
-	initGallery();
+  setHeader();
+  initMenu();
+  initFavs();
+  initSvg();
+  initGallery();
 
-	$(window).on('resize', function()
-	{
-		setHeader();
+  $(window).on('resize', function()
+  {
+    setHeader();
 
-		setTimeout(function()
-		{
-			$(window).trigger('resize.px.parallax');
-		}, 375);
-	});
+    setTimeout(function()
+    {
+      $(window).trigger('resize.px.parallax');
+    }, 375);
+  });
 
-	$(document).on('scroll', function()
-	{
-		setHeader();
-	});
+  $(document).on('scroll', function()
+  {
+    setHeader();
+  });
 
-	/* 
+  /*
 
-	2. Set Header
+  2. Set Header
 
-	*/
+  */
 
-	function setHeader()
-	{
-		var header = $('.header');
+  function setHeader()
+  {
+    var header = $('.header');
 
-		if($(window).scrollTop() > 91)
-		{
-			header.addClass('scrolled');
-		}
-		else
-		{
-			header.removeClass('scrolled');
-		}
-	}
+    if($(window).scrollTop() > 91)
+    {
+      header.addClass('scrolled');
+    }
+    else
+    {
+      header.removeClass('scrolled');
+    }
+  }
 
-	/* 
+  /*
 
-	3. Init Menu
+  3. Init Menu
 
-	*/
+  */
 
-	function initMenu()
-	{
-		if($('.menu').length)
-		{
-			var menu = $('.menu');
-			var hamburger = $('.hamburger');
-			var close = $('.menu_close');
+  function initMenu()
+  {
+    if($('.menu').length)
+    {
+      var menu = $('.menu');
+      var hamburger = $('.hamburger');
+      var close = $('.menu_close');
 
-			hamburger.on('click', function()
-			{
-				menu.toggleClass('active');
-			});
+      hamburger.on('click', function()
+      {
+        menu.toggleClass('active');
+      });
 
-			close.on('click', function()
-			{
-				menu.toggleClass('active');
-			});
-		}
-	}
+      close.on('click', function()
+      {
+        menu.toggleClass('active');
+      });
+    }
+  }
 
-	/* 
+  /*
 
-	4. Init Favs
+  4. Init Favs
 
-	*/
+  */
 
-	function initFavs()
-	{
-		if($('.show_fav_icon').length)
-		{
-			var icons = $('.show_fav_icon');
-			icons.each(function()
-			{
-				var icon = $(this);
-				icon.on('click', function()
-				{
-					if(icon.hasClass('active'))
-					{
-						icon.removeClass('active');
-					}
-					else
-					{
-						icon.addClass('active');
-					}
-				});
-			});
-		}
-	}
+  function initFavs()
+  {
+    if($('.show_fav_icon').length)
+    {
+      var icons = $('.show_fav_icon');
+      icons.each(function()
+      {
+        var icon = $(this);
+        icon.on('click', function()
+        {
+          if(icon.hasClass('active'))
+          {
+            icon.removeClass('active');
+          }
+          else
+          {
+            icon.addClass('active');
+          }
+        });
+      });
+    }
+  }
 
-	/* 
+  /*
 
-	5. Init SVG
+  5. Init SVG
 
-	*/
+  */
 
-	function initSvg()
-	{
-		if($('img.svg').length)
-		{
-			jQuery('img.svg').each(function()
-			{
-				var $img = jQuery(this);
-				var imgID = $img.attr('id');
-				var imgClass = $img.attr('class');
-				var imgURL = $img.attr('src');
+  function initSvg()
+  {
+    if($('img.svg').length)
+    {
+      jQuery('img.svg').each(function()
+      {
+        var $img = jQuery(this);
+        var imgID = $img.attr('id');
+        var imgClass = $img.attr('class');
+        var imgURL = $img.attr('src');
 
-				jQuery.get(imgURL, function(data)
-				{
-					// Get the SVG tag, ignore the rest
-					var $svg = jQuery(data).find('svg');
+        jQuery.get(imgURL, function(data)
+        {
+          // Get the SVG tag, ignore the rest
+          var $svg = jQuery(data).find('svg');
 
-					// Add replaced image's ID to the new SVG
-					if(typeof imgID !== 'undefined') {
-					$svg = $svg.attr('id', imgID);
-					}
-					// Add replaced image's classes to the new SVG
-					if(typeof imgClass !== 'undefined') {
-					$svg = $svg.attr('class', imgClass+' replaced-svg');
-					}
+          // Add replaced image's ID to the new SVG
+          if(typeof imgID !== 'undefined') {
+          $svg = $svg.attr('id', imgID);
+          }
+          // Add replaced image's classes to the new SVG
+          if(typeof imgClass !== 'undefined') {
+          $svg = $svg.attr('class', imgClass+' replaced-svg');
+          }
 
-					// Remove any invalid XML tags as per http://validator.w3.org
-					$svg = $svg.removeAttr('xmlns:a');
+          // Remove any invalid XML tags as per http://validator.w3.org
+          $svg = $svg.removeAttr('xmlns:a');
 
-					// Replace image with new SVG
-					$img.replaceWith($svg);
-				}, 'xml');
-			});
-		}	
-	}
+          // Replace image with new SVG
+          $img.replaceWith($svg);
+        }, 'xml');
+      });
+    }
+  }
 
-	/* 
+  /*
 
-	6. Init Gallery
+  6. Init Gallery
 
-	*/
+  */
 
-	function initGallery()
-	{
-		if($('.gallery_item').length)
-		{
-			$('.colorbox').colorbox(
-			{
-				rel:'colorbox',
-				photo: true,
-				maxWidth:'95%',
-				maxHeight:'95%'
-			});
-		}
-	}
+  function initGallery()
+  {
+    if($('.gallery_item').length)
+    {
+      $('.colorbox').colorbox(
+      {
+        rel:'colorbox',
+        photo: true,
+        maxWidth:'95%',
+        maxHeight:'95%'
+      });
+    }
+  }
 
 });
