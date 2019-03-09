b39fbdfd92fda805e1a8d6aa92b625e8bdf3b9fd
Sergio <281540+sergio97@users.noreply.github.com>
2019-03-09 12:45:34 -0500

    Add episode 7

--- js/episodes.js
+++ js/episodes.js
@@ -379,6 +379,35 @@ $(document).ready(function()
         warningAlerts: false
       });
 
+      $("#jplayer_7").jPlayer({
+        ready: function () {
+          $(this).jPlayer("setMedia", {
+            title:"Episode 7",
+              artist:"",
+              mp3:"episodes/ttp-episode7-nick-aleks-ophe-chan-brian-bourne.mp3"
+          });
+        },
+        play: function() { // To avoid multiple jPlayers playing together.
+          $(this).jPlayer("pauseOthers");
+        },
+        swfPath: "plugins/jPlayer",
+        supplied: "mp3",
+        cssSelectorAncestor: "#jp_container_7",
+        wmode: "window",
+        globalVolume: false,
+        useStateClassSkin: true,
+        autoBlur: false,
+        smoothPlayBar: true,
+        keyEnabled: true,
+        solution: 'html',
+        preload: 'metadata',
+        volume: 0.7,
+        muted: false,
+        backgroundColor: '#000000',
+        errorAlerts: false,
+        warningAlerts: false
+      });
+
     }
   }
 
