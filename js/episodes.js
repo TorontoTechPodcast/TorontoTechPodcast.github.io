88603563fd511d311002ccb1ef73f18ce2e7fac7
Sergio <281540+sergio97@users.noreply.github.com>
2019-04-28 12:05:18 -0400

    Add episode 8 properly

--- js/episodes.js
+++ js/episodes.js
@@ -408,6 +408,35 @@ $(document).ready(function()
         warningAlerts: false
       });
 
+      $("#jplayer_8").jPlayer({
+        ready: function () {
+          $(this).jPlayer("setMedia", {
+            title:"Episode 8",
+              artist:"",
+              mp3:"episodes/ttp-episode8-games-poetry-and-podcast-review.mp3"
+          });
+        },
+        play: function() { // To avoid multiple jPlayers playing together.
+          $(this).jPlayer("pauseOthers");
+        },
+        swfPath: "plugins/jPlayer",
+        supplied: "mp3",
+        cssSelectorAncestor: "#jp_container_8",
+        wmode: "window",
+        globalVolume: false,
+        useStateClassSkin: true,
+        autoBlur: false,
+        smoothPlayBar: true,
+        keyEnabled: true,
+        solution: 'html',
+        preload: 'metadata',
+        volume: 0.7,
+        muted: false,
+        backgroundColor: '#000000',
+        errorAlerts: false,
+        warningAlerts: false
+      });
+
     }
   }
 
