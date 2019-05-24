8da03c6beec6ae87ddcc3f2d1b616b734d414de4
Sergio <281540+sergio97@users.noreply.github.com>
2019-05-24 16:21:38 -0400

    Add ep9 to list

--- js/episodes.js
+++ js/episodes.js
@@ -437,6 +437,35 @@ $(document).ready(function()
         warningAlerts: false
       });
 
+      $("#jplayer_9").jPlayer({
+        ready: function () {
+          $(this).jPlayer("setMedia", {
+            title:"Episode 9",
+              artist:"",
+              mp3:"episodes/ttp-episode9-enterprise-it-snakes-and-ladders.mp3"
+          });
+        },
+        play: function() { // To avoid multiple jPlayers playing together.
+          $(this).jPlayer("pauseOthers");
+        },
+        swfPath: "plugins/jPlayer",
+        supplied: "mp3",
+        cssSelectorAncestor: "#jp_container_9",
+        wmode: "window",
+        globalVolume: false,
+        useStateClassSkin: true,
+        autoBlur: false,
+        smoothPlayBar: true,
+        keyEnabled: true,
+        solution: 'html',
+        preload: 'metadata',
+        volume: 0.7,
+        muted: false,
+        backgroundColor: '#000000',
+        errorAlerts: false,
+        warningAlerts: false
+      });
+
     }
   }
 
