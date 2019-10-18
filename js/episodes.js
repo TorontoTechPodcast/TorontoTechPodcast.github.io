ee67a0f103e0187909196d3ac1e9fbc9af2055aa
Sergio <281540+sergio97@users.noreply.github.com>
2019-10-18 09:33:24 -0400

    Add ep13

--- js/episodes.js
+++ js/episodes.js
@@ -553,6 +553,35 @@ $(document).ready(function()
         warningAlerts: false
       });
 
+      $("#jplayer_13").jPlayer({
+        ready: function () {
+          $(this).jPlayer("setMedia", {
+            title:"Episode 13",
+              artist:"",
+              mp3:"episodes/ttp-013-data-science-will-change-your-business.mp3"
+          });
+        },
+        play: function() { // To avoid multiple jPlayers playing together.
+          $(this).jPlayer("pauseOthers");
+        },
+        swfPath: "plugins/jPlayer",
+        supplied: "mp3",
+        cssSelectorAncestor: "#jp_container_13",
+        wmode: "window",
+        globalVolume: false,
+        useStateClassSkin: true,
+        autoBlur: false,
+        smoothPlayBar: true,
+        keyEnabled: true,
+        solution: 'html',
+        preload: 'metadata',
+        volume: 0.7,
+        muted: false,
+        backgroundColor: '#000000',
+        errorAlerts: false,
+        warningAlerts: false
+      });
+
     }
   }
 
